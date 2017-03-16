b3b13e05e93a5bebfa33dc3aa8fa7ae9d59f4dd4
chenxiao07150808 <932900547@qq.com>
2017-03-16 15:14:57 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/s07150808/listview/MainActivity.java
@@ -0,0 +1,71 @@
+package cn.edu.gdmec.s07150808.listview;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+
+public class MainActivity extends Activity implements ReFlashListView.IReflashListener {
+	ArrayList<ApkEntity> apk_list;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		setData();
+		showList(apk_list);
+	}
+
+	MyAdapter adapter;
+	ReFlashListView listview;
+	private void showList(ArrayList<ApkEntity> apk_list) {
+		if (adapter == null) {
+			listview = (ReFlashListView) findViewById(R.id.listview);
+			listview.setInterface(this);
+			adapter = new MyAdapter(this, apk_list);
+			listview.setAdapter(adapter);
+		} else {
+			adapter.onDateChange(apk_list);
+		}
+	}
+
+	private void setData() {
+		apk_list = new ArrayList<ApkEntity>();
+		for (int i = 0; i < 10; i++) {
+			ApkEntity entity = new ApkEntity();
+			entity.setName("默认数据");
+			entity.setDes("这是一个神奇的应用");
+			entity.setInfo("50w用户");
+			apk_list.add(entity);
+		}
+	}
+
+	private void setReflashData() {
+		for (int i = 0; i < 2; i++) {
+			ApkEntity entity = new ApkEntity();
+			entity.setName("刷新数据");
+			entity.setDes("这是一个神奇的应用");
+			entity.setInfo("50w用户");
+			apk_list.add(0,entity);
+		}
+	}
+	@Override
+	public void onReflash() {
+		// TODO Auto-generated method stub\
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+
+				setReflashData();
+				showList(apk_list);
+				listview.reflashComplete();
+			}
+		}, 2000);
+		
+	}
+}
